a2bdf45505e2627ce835d96d9d107868b5b7c451
Team Base Computer <corjenmn@gmail.com>
2017-10-28 21:06:35 -0500

    Initial Version
    Initial version with some programming done.

---
+++ ftc_app-3.4/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgramingROBOT20172018.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
+
+/**
+ * Created by FireStormers on 10/22/2017.
+ */
+@TeleOp
+public class ProgramingROBOT20172018 extends LinearOpMode {
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+    private ColorSensor sensorColor;
+
+    @Override
+    public void runOpMode() {
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        servoTest = hardwareMap.get(Servo.class, "servoTest");
+        sensorColor = hardwareMap.get(ColorSensor.class, "sensorColorRange");
+
+        //hsvValues
+        float hsvValues[] ={0F, 0F, 0F};
+
+        // set digital channel to input mode
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
+        servoTest.setPosition(0);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()){
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+            telemetry.addData("Distance (cm)", sensorColorRange.getDistance(DistanceUnit.CM));
+
+            //Color sensor stuff
+            Color.RGBToHSV((int) (sensorColor.red()),
+                    (int) (sensorColor.green()),
+                    (int) (sensorColor.blue()),
+                    hsvValues);
+            //send the info back to driver station using telemetry function.
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+
+            //run until the end of the match (driver presses STOP)
+            //double tgtpower = 0
+                //tgtpower = -this.gamepad1.left_stick_y
+                //motorTest.setPower(tgtpower)
+            // check to see if we need to move the servo.
+            if(gamepad1.y) {
+                //move to 0 degrees.
+                servoTest.setPosition(0);
+            } else if (gamepad1.x) {
+                //move to 90 degrees.
+                servoTest.setPosition(0.5);
+            } else if (gamepad1.a) {
+                //move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+
+            // is button pressed?
+            if (digitalTouch.getState() == false) {
+                // button is pressed.
+                telemetry.addData("Button", "PRESSED");
+            } else {
+                // button is not pressed.
+                telemetry.addData("Button", "NOT PRESSED");
+            }
+
+            // If we see the blue jewel we will move forward and hit it off.
+            telemetry.addData("teamcode", "red");
+            if (sensorColor.blue() > sensorColor.red()) {
+                //
+                telemetry.addData("move direction", "forward");
+            } else {
+                // button is not pressed.
+                telemetry.addData("move direction", "backwards");
+            }
+
+
+            telemetry.addData("Servo Position", servoTest.getPosition());
+        }
+    }
+}
+
+
